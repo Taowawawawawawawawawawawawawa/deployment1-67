8926f9a96839300f914ff216c44ab3ee4102477e
Nungnon <Pisit_ch@cmu.ac.th>
2024-10-09 21:53:39 +0700

    yum

---
+++ src/index.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.BMI = BMI;
+function BMI(weight, height) {
+    if (weight <= 0)
+        throw new Error("Weight must be greater than 0");
+    if (height <= 0)
+        throw new Error("Height must be greater than 0");
+    return (weight / (height * height)).toFixed(2);
+}
+const weight = 60;
+const height = 1.75;
+console.log(`Your weight: ${weight} kg.\nYour height: ${height} m.\nYour BMI: ${BMI(weight, height)}`);
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-
 export function BMI(weight: number, height: number): string {
+  if (weight <= 0) throw new Error("Weight must be greater than 0");
   if (height <= 0) throw new Error("Height must be greater than 0");
   return (weight / (height * height)).toFixed(2);
 }
